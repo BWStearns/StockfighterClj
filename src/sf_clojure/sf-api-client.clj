d4bc618b58ef56517324ca19f9522cc6123c30d0
Brian Stearns <brianw.stearns@gmail.com>
2016-02-28 03:15:25 -0500

    made naming more clojuric(?) and actually bothered to make it compile. Woohoo.

--- src/sf_clojure/sf-api-client.clj
+++ src/sf_clojure/sf-api-client.clj
@@ -1,4 +1,4 @@
-(ns sf-clojure.sf_api_client
+(ns sf-clojure.sf-api-client
   (:gen-class))
 
 (require '[org.httpkit.client :as http])
@@ -41,7 +41,7 @@
 (defn sf-server-is-alive []
 	"Verifies that the game server is still working"
 	(let 
-		[{:keys [status headers body error] :as resp} @(call-api :is-alive)]
+		[{:keys [status headers body error] :as resp} @(http/get "https://api.stockfighter.io/ob/api/heartbeat")]
 		(= (:ok (json/read-json body)) true)))
 
 (defn get-quote
@@ -89,9 +89,9 @@
 	orderType: ['limit'|'market'|'fill-or-kill'|'immediate-or-cancel']
 	"
 	; https://api.stockfighter.io/ob/api/venues/:venue/stocks/:stock/orders
-	(http/post 
-		(str "https://api.stockfighter.io/ob/api/venues/" venue "/stocks/" stock "/orders")
-		(into req-opts {:body (json/encode order)}))
+	@(http/post 
+		(str "https://api.stockfighter.io/ob/api/venues/" (get order "venue") "/stocks/" (get order "stock") "/orders")
+		(into req-opts {:body (json/write-str order)}))
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -110,7 +110,7 @@
 
 (defn mk-ex-ticker
 	[acct venue]
-	(ws-http/w
+	(println "unimplemented"))
 
 (defn mk-ex-execution-ticker
 	[acct venue]
@@ -122,7 +122,7 @@
 	(ws-http/websocket-client 
 		(str "wss://api.stockfighter.io/ob/api/ws/" acct "/venues/" venue "/executions/stocks/" stock)))
 
-(def record-to [record-atom]
+(defn record-to [record-atom]
 	; Switch to update instead of replace with last
 	(fn [msg]
 		(reset! record-atom msg)))
@@ -161,27 +161,8 @@
 ;;;;;;;;;;;;;; COMPOSITES ;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(let [foo 3 bar 6 baz (fn [] (* bar 10)) a 5] (baz))
 
-; (defn block-sell [acct venue stock qty price-tolerance]
-; 	"Price tolerance is the % increase to tolerate before letting the market cool off.
-; 	Histories must be most recent at 0 index"
-; 	(let [
-; 		; State tracking shit
-; 		order-complete-or-failed (atom false)
-; 		ticker-history (atom [])
-; 		open-orders (atom [])
-; 		remaining-to-sell (atom qty)
-; 		; Save some parens
-; 		get-all-orders #(all-order-statuses venue acct)
-; 		]
-
-; 		(while (not order-complete-or-failed)
-; 			(let [
-; 				current-quote (get-quote venue stock)
-; 				orders (get-all-orders)
-; 				]
-				
-; 				))))
 
 (defn stupid-block-sell [acct venue stock qty]
 	"This method assumes your fellow market participants are criminally stupid."
@@ -189,9 +170,7 @@
 		; State tracking shit
 		order-complete-or-failed (atom false)
 		remaining-to-sell (atom qty)
-		last-quote-time (atom nil)
-		; Save some parens
-		get-all-orders #(all-order-statuses venue acct)
+		lq-time (atom nil)
 		fok-order (into default-order {
 			"direction" "buy"
 			"venue" venue
@@ -199,15 +178,17 @@
 			"account" acct
 			"orderType" "fill-or-kill"
 			})
+		get-all-orders (fn [] (all-order-statuses venue acct))
 		]
 		(while (not order-complete-or-failed)
-			(let [
-				lq (json/read-str @last-quote)
-				]
-				()
-				(if ()
-					(place-order (into fok-order {:price (get lq "bid") :qty (get lq "bidSize")}))
+			(let [lq (json/read-str @last-quote)]
 
+				(if (and lq (not= lq-time (get lq "quoteTime")))
+					(
+						(reset! lq-time (get lq "quoteTime"))
+						(println (place-order (into fok-order {:price (get lq "bid") :qty (get lq "bidSize")}))))
+					(
+						println "Do Nothing")
 					)
 						))))
 
